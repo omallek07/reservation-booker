b8ab4584c4e2497286924188de5bd45ae4f83179
Kevin O'Malley <omallek@gmail.com>
2026-06-27 05:42:21 -0400

    feat: add auth / user base app files

---
+++ apps/auth/src/users/users.module.ts
@@ -0,0 +1,18 @@
+import { DatabaseModule } from '@app/common';
+import { Module } from '@nestjs/common';
+import { UserDocument } from 'apps/auth/src/users/models/user.schema';
+import { UsersRepository } from 'apps/auth/src/users/users.repository';
+import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
+
+@Module({
+  imports: [
+    DatabaseModule,
+    DatabaseModule.forFeature([
+      { name: UserDocument.name, schema: UserDocument },
+    ]),
+  ],
+  controllers: [UsersController],
+  providers: [UsersService, UsersRepository],
+})
+export class UsersModule {}

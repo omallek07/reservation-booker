7d57f5e25a15d1bfdf7043cc159d9e44ca05c458
Kevin O'Malley <omallek@gmail.com>
2026-06-27 06:08:32 -0400

    chore: add dockerfile config for auth

--- apps/auth/src/users/users.module.ts
+++ apps/auth/src/users/users.module.ts
@@ -1,15 +1,15 @@
 import { DatabaseModule } from '@app/common';
 import { Module } from '@nestjs/common';
-import { UserDocument } from 'apps/auth/src/users/models/user.schema';
-import { UsersRepository } from 'apps/auth/src/users/users.repository';
+import { UserDocument, UserSchema } from './models/user.schema';
 import { UsersController } from './users.controller';
+import { UsersRepository } from './users.repository';
 import { UsersService } from './users.service';
 
 @Module({
   imports: [
     DatabaseModule,
     DatabaseModule.forFeature([
-      { name: UserDocument.name, schema: UserDocument },
+      { name: UserDocument.name, schema: UserSchema },
     ]),
   ],
   controllers: [UsersController],
--- apps/reservations/src/main.ts
+++ apps/reservations/src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
     }),
   );
   app.useLogger(app.get(Logger));
-  await app.listen(process.env.port ?? 3000);
+  await app.listen(3000);
 }
 
 bootstrap();
